13283c98c54bef803d94581649bff974963e964e
tcerdaITBA <tcerda@itba.edu.ar>
2018-03-10 19:07:42 -0300

    Validación de parametros en refine

--- TP1/@network/refine.m
+++ TP1/@network/refine.m
@@ -3,6 +3,11 @@ function net = refine(net, input_pattern, expected)
     % the input_pattern and expected values given
     % input_pattern: vector of values given to the input neurons
     % expected: expected output given the input_pattern
+
+    if (length(expected) != rows(net.weights))
+        error('@network/refine: length of expected values vector does not match the number of output neurons')
+    end
+
     actual = activate(net, input_pattern);
     delta = (expected - actual) .* net.df_activation(actual);
     net.weights = net.weights + net.lr * delta' * [-1 input_pattern];
