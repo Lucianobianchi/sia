c9a1cbdf7832f0b02cfd68effdf7b86a5f2b8e1e
Luciano Bianchi <lucianogbianchi@gmail.com>
2018-03-14 18:01:48 -0300

    split sets

---
+++ TP1/split_sets.m
@@ -0,0 +1,19 @@
+function [train_set train_expected test_set test_expected] = split_sets(input_set, expected, test_ratio, seed)
+    li = rows(input_set);
+    le = length(expected);
+    if (li != le)
+        error('split_sets: length of expected values vector (%d) does not match the number of input patterns (%d)', le, li);
+    end
+
+    rand("seed", seed);
+    shuffled = randperm(length(input_set));
+    test_limit = round(length(input_set) * test_ratio);
+
+    test_index = shuffled(1:test_limit);
+    test_set = input_set(test_index, :);
+    test_expected = expected(test_index);
+
+    train_index = shuffled(test_limit + 1:end);
+    train_set = input_set(train_index, :);
+    train_expected = expected(train_index);
+endfunction
