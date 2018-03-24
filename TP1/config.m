b86e2f21d646ecd9e70b91d9dc95d3a1448a8277
tcerdaITBA <tcerda@itba.edu.ar>
2018-03-24 17:27:54 -0300

    test 2 rehecho

--- TP1/config.m
+++ TP1/config.m
@@ -1,18 +1,18 @@
 %% Network config
 n_inputs = 2;
 n_outputs = 1;
-hidden_layers = [40 20];
-lr = 0.05;
+hidden_layers = [30];
+lr = 0.01;
 f_activation_name = 'tanh';
 slope = 1;
-initialization = 'uniform_one';
+initialization = 'uniform';
 input_lower_bound = -1;
 input_upper_bound = 1;
 
 %% Train config
-batch_size = 500;
+batch_size = 5;
 epochs = 1;
-algorithm = 'momentum';      % 'momentum' 'adaptive' 'vanilla'
+algorithm = 'vanilla';      % 'momentum' 'adaptive' 'vanilla'
 alfa = 0.5;                  % momentum config
 cost_interval = 1;          % adaptive config
 inc_steps = 5;               % adaptive config
@@ -28,7 +28,7 @@ n_toperrors = 10;
 
 %% Set config
 filename = 'terrain02.data';
-seed = 8098;  % use time() for a different seed each config
+seed = 42;  % use time() for a different seed each config
 test_ratio = 0.35;
 
 %Train Test config
