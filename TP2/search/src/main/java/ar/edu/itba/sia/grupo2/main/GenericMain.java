f946f3f8d43c6cccc4cd3bad2ba13fe0f8fd1949
Tomas Cerda <Tomas.Cerda@ibm.com>
2018-04-29 15:53:43 -0300

    Restore main

--- TP2/search/src/main/java/ar/edu/itba/sia/grupo2/main/GenericMain.java
+++ TP2/search/src/main/java/ar/edu/itba/sia/grupo2/main/GenericMain.java
@@ -1,20 +1,30 @@
 package ar.edu.itba.sia.grupo2.main;
 
-
-
 import ar.com.itba.sia.Problem;
+import ar.edu.itba.sia.grupo2.engine.Node;
 import ar.edu.itba.sia.grupo2.engine.Search;
 import ar.edu.itba.sia.grupo2.engine.informed.AStar;
-import ar.edu.itba.sia.grupo2.problem.SenkuBoard;
-import ar.edu.itba.sia.grupo2.problem.SenkuBoardLoader;
-import ar.edu.itba.sia.grupo2.problem.SenkuProblem;
-import ar.edu.itba.sia.grupo2.problem.heuristic.IsolatedPegs;
+import ar.edu.itba.sia.grupo2.problem.*;
+import ar.edu.itba.sia.grupo2.problem.heuristic.*;
+import ar.edu.itba.sia.grupo2.utils.EngineStats;
+
+import java.util.Optional;
 
 public class GenericMain {
-    public static void main(String[] args) {
-        final Problem<SenkuBoard> problem = new SenkuProblem(SenkuBoardLoader.load("boards/board4.txt"));
+        public static void main(String[] args) {
+                final Problem<SenkuBoard> problem = new SenkuProblem(SenkuBoardLoader.load("boards/board4.txt"));
 
-        final Search<SenkuBoard> search = new AStar<>(new IsolatedPegs());
-        //final Search<SenkuBoard> search = new DFS<>();
-    }
+                final Search<SenkuBoard> search = new AStar<>(new IsolatedPegs());
+                //final Search<SenkuBoard> search = new DFS<>();
+                //final Search<SenkuBoard> search = new BFS<>();
+                final Optional<Node<SenkuBoard>> node = search.graphSearch(problem);
+
+                final EngineStats stats = search.getStats();
+
+                System.out.println("Time elapsed: " + stats.getTimeElapsed());
+                System.out.println("Expansions: " + stats.getLevelExpansions());
+                System.out.println("Total expansions: " + stats.getTotalExpansions());
+                node.ifPresent(n -> System.out.println("Solution level: " + n.getLevel()));
+                System.out.println(node);
+        }
 }
