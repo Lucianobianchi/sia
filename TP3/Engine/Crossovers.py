043cc64e7aed800a78188fc0aab26516344e46a9
tcerdaITBA <tcerda@itba.edu.ar>
2018-05-19 15:45:39 -0300

    Two point crossover

--- TP3/Engine/Crossovers.py
+++ TP3/Engine/Crossovers.py
@@ -2,15 +2,28 @@ from random import Random
 
 random = Random() # TODO: seed
 
-def _one_point(pair):
+def _one_point_crossover(pair):
     locus = random.randint(0, pair[0].genes_count)
-    a = pair[0].genes[:locus] + pair[1].genes[locus:]
-    b = pair[1].genes[:locus] + pair[0].genes[locus:]
+    p0 = pair[0]
+    p1 = pair[1]
+    a = p0.genes[:locus] + p1.genes[locus:]
+    b = p1.genes[:locus] + p0.genes[locus:]
     return [a, b]
 
+def _two_point_crossover(pair):
+    gen_count = pair[0].genes_count
+    p0 = pair[0]
+    p1 = pair[1]
+    r1 = random.randint(1, gen_count - 2)
+    r2 = random.randint(r1 + 1, gen_count - 1)
+    a = p0.genes[:r1] + p1.genes[r1:r2] + p0.genes[r2:]
+    b = p1.genes[:r1] + p0.genes[r1:r2] + p1.genes[r2:]
+    return [a, b]
+    
+
 strategies = {
-    'one_point': _one_point,
-    'two_point': None,
+    'one_point': _one_point_crossover,
+    'two_point': _two_point_crossover,
     'anular': None,
     'uniform_cross': None
 }
