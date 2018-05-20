4e0fec1624e7c8e58598e79b0d85d96f52ed6cca
tcerdaITBA <tcerda@itba.edu.ar>
2018-05-20 12:13:17 -0300

    Quitando instancia Random en Selectors

--- TP3/Engine/Selectors.py
+++ TP3/Engine/Selectors.py
@@ -1,26 +1,25 @@
-from random import Random
+from random import random, sample, choices
 from bisect import bisect
 from itertools import accumulate
 
 fit_getter = lambda i: i.fitness
-random = Random() # TODO: seed
 
 def _elite_selector(group, select_count):
     ranked = sorted(group, key = fit_getter, reverse = True)
     return ranked[:select_count]
 
 def _random_selector(group, select_count):
-    return random.sample(group, select_count)
+    return sample(group, select_count)
 
 def _roulette_selector(group, select_count):
     fitnesses = [i.fitness for i in group]
-    return random.choices(group, weights = fitnesses, k = select_count)
+    return choices(group, weights = fitnesses, k = select_count)
 
 def _universal_selector(group, select_count):
     acum = _acum_rel_fitness(group)
     k = select_count
-    r = random.random()
-    return [group[bisect(acum, (r + j) / k)] for j in range(0, k)] # Could be more efficient without calling bisect each time
+    r = random()
+    return [group[bisect(acum, (r + j) / k)] for j in range(0, k)]
 
 def _acum_rel_fitness(group):
     total_fitness = sum(i.fitness for i in group)
@@ -29,13 +28,13 @@ def _acum_rel_fitness(group):
 
 # TODO: considerar tener algo que sea como 'load_selectors(config)' que settee parámetros extras como la m de tournament
 def _tournament_det_selector(group, select_count, m = 2):
-    return [max(random.sample(group, m), key = fit_getter) for _ in range(select_count)]
+    return [max(sample(group, m), key = fit_getter) for _ in range(select_count)]
 
 def _tournament_prob_selector(group, select_count):
-    return [_pick_winner(random.sample(group, 2)) for _ in range(select_count)]
+    return [_pick_winner(sample(group, 2)) for _ in range(select_count)]
 
 def _pick_winner(contenders):
-    r = random.random()
+    r = random()
     return max(contenders, key = fit_getter) if r < 0.75 else min(contenders, key = fit_getter)
 
 strategies = {
