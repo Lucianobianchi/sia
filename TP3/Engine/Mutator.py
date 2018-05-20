cfed9f4c344eb3fbbcd89699b4f0c62021e6049e
tcerdaITBA <tcerda@itba.edu.ar>
2018-05-20 12:37:12 -0300

    Quitando instancia Random en Mutator

--- TP3/Engine/Mutator.py
+++ TP3/Engine/Mutator.py
@@ -1,9 +1,7 @@
-from random import Random
-
-random = Random() # TODO: seed
+from random import random
 
 def mutate_genes(genes, mutate_prob):
     for i, gene in enumerate(genes):
-        if random.random() < mutate_prob:
+        if random() < mutate_prob:
             genes[i] = gene.mutate()
     return genes
